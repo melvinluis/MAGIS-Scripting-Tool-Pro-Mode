99eb14ec573609f814972ea94808191ae894a7b7
Melvin Luis Mendoza <melvinluismendoza@gmail.com>
2017-09-04 11:06:43 +0800

    Encapsulated flag addition in flag management screen

---
+++ MAGIS Scripting Tool - Pro Mode/Assets/FlagManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlagManager : MonoBehaviour {
+
+    public InputField f_inputfield; // Name input when adding flags
+    public RectTransform f_management; // Flag management panel
+    public Image disabler; // Disables buttons behind popup panel
+
+    private ArrayList flags = new ArrayList(); // Store all flags here
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public ArrayList GetFlags() {
+        return flags;
+    }
+
+    public void AddFlagDialogue() {
+        /* Enables the text box and add button when adding new flags 
+         */
+        f_inputfield.gameObject.SetActive(true);
+    }
+
+    bool CheckFlagName(string name) {
+        // TODO: do checks here
+        return true;
+    }
+
+    public void ManageFlags() {
+        /* Displays the flags for users to manage.
+         * */
+
+        if (!f_management.gameObject.activeInHierarchy) {
+            disabler.gameObject.SetActive(true);
+            f_management.gameObject.SetActive(true);
+
+            // TODO: figure out how to display flag management
+            for (int i = 0; i < flags.Count; i++) {
+                Debug.Log(flags[i]);
+            }
+        }
+        else {
+            disabler.gameObject.SetActive(false);
+            f_management.gameObject.SetActive(false);
+        }
+    }
+
+    public void AddFlag() {
+        /* Adds a new flag */
+
+        // check if the string is valid
+
+        // if valid, add
+        flags.Add(f_inputfield.text);
+        // reset
+        f_inputfield.text = ""; // set default values because this object gets recycled
+        f_inputfield.gameObject.SetActive(false); // disable
+    }
+}
--- MAGIS Scripting Tool - Pro Mode/Assets/Scripts/Manager.cs
+++ MAGIS Scripting Tool - Pro Mode/Assets/Scripts/Manager.cs
@@ -17,13 +17,6 @@ public class Manager : MonoBehaviour {
     /* Other Objects */
     public Image disabler; // Disables the buttons on the main editing page by overlaying an image object
 
-    /* Data Management */
-    private ArrayList flags = new ArrayList(); // ArrayList that will contain all of the flags in string format
-
-    /* Flag Management */
-    public InputField f_inputfield; // Name input when adding flags
-    public RectTransform f_management; // Flag management panel
-
     // Use this for initialization
     void Start () {
 
@@ -33,47 +26,4 @@ public class Manager : MonoBehaviour {
 	void Update () {
 		
 	}
-
-    /* Flags Management */
-    public void AddFlag() {
-        /* Adds a new flag */
-
-        // check if the string is valid
-
-        // if valid, add
-        flags.Add(f_inputfield.text);
-        // reset
-        f_inputfield.text = ""; // set default values because this object gets recycled
-        f_inputfield.gameObject.SetActive(false); // disable
-    }
-
-    bool CheckFlagName(string name) {
-        // TODO: do checks here
-        return true;
-    }
-
-    public void AddFlagDialogue() {
-        /* Enables the text box and add button when adding new flags 
-         */
-        f_inputfield.gameObject.SetActive(true);
-    }
-
-    public void ManageFlags() {
-        /* Displays the flags for users to manage.
-         * */
-
-        if (!f_management.gameObject.activeInHierarchy) {
-            disabler.gameObject.SetActive(true);
-            f_management.gameObject.SetActive(true);
-
-            // TODO: figure out how to display flag management
-            for(int i = 0; i < flags.Count; i++) {
-                Debug.Log(flags[i]);
-            }
-        }
-        else {
-            disabler.gameObject.SetActive(false);
-            f_management.gameObject.SetActive(false);
-        }
-    }
 }
