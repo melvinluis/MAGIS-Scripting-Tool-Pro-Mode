5e885dca4696e71f7d0ba415bcb8c9cc4edfd4e3
Work Laptop <melvinluismendoza@gmail.com>
2017-09-06 16:39:55 +0800

    More flag management UI

--- MAGIS Scripting Tool - Pro Mode/Assets/Scripts/FlagManager.cs
+++ MAGIS Scripting Tool - Pro Mode/Assets/Scripts/FlagManager.cs
@@ -9,6 +9,8 @@ public class FlagManager : MonoBehaviour {
     public RectTransform f_management; // Flag management panel
     public Image disabler; // Disables buttons behind popup panel
     public Image prompt; // The object used for error messages
+    public RectTransform content; // The scrollrect that displays all flags
+    public Button flagDisplayButton; // Button used to display flags in Flag Management
 
     private ArrayList flags = new ArrayList(); // Store all flags here
 
@@ -36,12 +38,29 @@ public class FlagManager : MonoBehaviour {
         /* Checks whether the submitted flag name is alphanumeric
          * Note that this also allows characters like åäö
          * */
-        for(int i = 0; i < name.Length; i++) {
+        bool valid = true;
+
+        // empty string
+        if(name == "") valid = false;
+
+        // special characters
+        for (int i = 0; i < name.Length; i++) {
             if (!char.IsLetterOrDigit(name[i])) {
-                prompt.GetComponent<ShowMessageDialogue>().RaiseError("Invalid flag name");
-                return false;
+                valid = false;
+                break;
             }
         }
+
+        // existing flags
+        for(int i = 0; i < flags.Count; i++) {
+            if(name == (string) flags[i]) valid = false;
+        }
+
+        if (!valid) {
+            prompt.GetComponent<ShowMessageDialogue>().RaiseError("Invalid flag name");
+            return false;
+        }
+
         return true;
     }
 
@@ -51,15 +70,13 @@ public class FlagManager : MonoBehaviour {
         if (!f_management.gameObject.activeInHierarchy) {
             disabler.gameObject.SetActive(true);
             f_management.gameObject.SetActive(true);
-
-            // TODO: figure out how to display all flags and all related flag management functions
-            for (int i = 0; i < flags.Count; i++) {
-                Debug.Log(flags[i]);
-            }
+            content.parent.parent.gameObject.transform.GetChild(1).GetComponent<Scrollbar>().value = 1; // reset the scrollbar scroll amount
+            DisplayFlags();
         }
         else {
             disabler.gameObject.SetActive(false);
             f_management.gameObject.SetActive(false);
+            f_inputfield.gameObject.SetActive(false);
         }
     }
 
@@ -69,9 +86,30 @@ public class FlagManager : MonoBehaviour {
         // check if the string is valid
         if (CheckFlagName(newflag)) {
             flags.Add(newflag); // add flag
-            f_inputfield.gameObject.SetActive(false); // disable
+            f_inputfield.gameObject.SetActive(false); // disable input field and button
+
+            Button temp = (Button) Instantiate(flagDisplayButton, content); // add new button to content list
+            temp.transform.GetChild(0).GetComponent<Text>().text = newflag; // set the button label
+            
+            temp.transform.localPosition = new Vector3(0, -(flags.Count-1)*30, 0); // check for position
+            content.sizeDelta = new Vector2(0, flags.Count * 30); // resize content rect
         }
         // reset
         f_inputfield.text = ""; // set default values because this object gets recycled
+    }
+
+    void DisplayFlags() {
+        /*
+         * Add flag
+         * Change Search bar
+         * Rename
+         * Delete
+         * Click manage flags XX
+         * Scrolling (auto) XX
+         * */
+
+        for(int i = 0; i < flags.Count; i++) {
+            
+        }
     }
 }
